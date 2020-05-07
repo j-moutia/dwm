64416b50d22eff4a09d3974be6af91468c3060b9
j-moutia <10386738+j-moutia@users.noreply.github.com>
2020-05-07 15:18:12 +0800

    Slightly increased font size.

--- config.h
+++ config.h
@@ -10,10 +10,10 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          =   {
-                                            "Terminus:size=8:antialias=true:autohint=true",      /* Regular font */
-                                            "Joypixels:size=8:antialias=true:autohint=true"      /* Color emojis */
+                                            "Terminus:size=9:antialias=true:autohint=true",      /* Regular font */
+                                            "Joypixels:size=11:antialias=true:autohint=true"      /* Color emojis */
                                         };
-static const char dmenufont[]       = "Terminus:size=8:antialias=true:autohint=true";
+static const char dmenufont[]       = "Terminus:size=9:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
